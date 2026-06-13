dcafc3f4a87785027c8c8aecfcd17a7af460ca69
ecara <edoardo.caramella@studenti.unimi.it>
2026-06-13 20:32:07 +0200

    modificato install.ps1

--- install.ps1
+++ install.ps1
@@ -1,3 +1,23 @@
+$configFile = ".\settings.json"
+
+if (Test-Path $configFile) {
+    $settings = Get-Content $configFile | ConvertFrom-Json
+    $modsDestination = $settings.modsPath
+}
+else {
+    $modsDestination = Read-Host "Inserisci il percorso della cartella mods"
+
+    @{
+        modsPath = $modsDestination
+    } | ConvertTo-Json | Set-Content $configFile
+}
+
+Copy-Item ".\*" `
+    -Destination $modsDestination `
+    -Recurse `
+    -Force `
+    -Exclude @("install.ps1","settings.json",".git","presets")
+
 $presetDir = "$env:APPDATA\SlayTheSpire2\card_editor\presets"
 
 if (!(Test-Path $presetDir)) {
@@ -9,5 +29,5 @@ Copy-Item `
 "$presetDir\Primo tentativo.json" `
 -Force
 
-Write-Host "Preset aggiornato."
+Write-Host "Installazione completata!"
 Pause
